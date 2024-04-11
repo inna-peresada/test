f7add3b117d78ac9f04d5f8dee8cf184e1525f95
Inna Peresada <inna.peresada@gmail.com>
2024-04-11 21:39:53 +0300

    updated_hw_8

---
+++ src/controllers/About.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace src\controllers;
+
+class About
+{
+    public function index()
+    {
+        echo "About";
+    }
+}
---
+++ src/controllers/Home.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace src\controllers;
+
+class Home
+{
+    public function index()
+    {
+        echo "Home";
+    }
+}
